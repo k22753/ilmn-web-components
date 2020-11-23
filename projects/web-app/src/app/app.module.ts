0132e20e68d02fd40f47cdd389aaa5510dab4e3e
sai <svsai92@gmail.com>
2020-11-23 06:36:19 -0800

    web-component lib mapped to we-application

---
+++ projects/web-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { WebComponentsLibModule } from '@ilmn/components-lib';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WebComponentsLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
